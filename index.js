38effdcbe0dc5b7a6f15ad8e91113d8cce92feee
Adriano Botelho <adrianosbotelho@gmail.com>
2018-08-27 23:15:02 -0300

    Express routes

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const app = express();
 
+const courses = [
+    {id: 1, name: 'course 1'},
+    {id: 2, name: 'course 2'},
+    {id: 3, name: 'course 3'}
+]
+
 app.get('/',(req, res) => {
     res.send('hello World!!!');
 });
 
 
 app.get('/api/courses',(req, res) => {
-    res.send([1, 2, 3]);
+    res.send(courses);
+});
+
+
+app.get('/api/courses/:id',(req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('Curso nao encontrado');
+    res.send(course);
+});
+
+
+app.get('/api/posts/:year/:month',(req, res) => {
+    res.send(req.query);
 });
 
 //port
