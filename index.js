ab621c65308f234a0034c675b79c3cee6d0600b5
Adriano Botelho <adrianosbotelho@gmail.com>
2018-08-29 22:52:00 -0300

    joi

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
+
 
 const courses = [
     {id: 1, name: 'course 1'},
@@ -16,6 +18,20 @@ app.get('/api/courses',(req, res) => {
     res.send(courses);
 });
 
+
+app.post('/api/courses', (req, res) => {
+    if (!req.body.name || req.body.length < 3 ){
+        // 400 bad request
+        res.status(400).send('Name is required and should be minimum 3 caracter.');
+        return;
+    }
+    const course = {
+        id : courses.length + 1,
+        name : req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+});
 
 app.get('/api/courses/:id',(req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
