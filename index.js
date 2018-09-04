58b3544623f2f569afe2d427a324536b24955788
Adriano Botelho <adrianosbotelho@gmail.com>
2018-09-03 22:38:41 -0300

    Api

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const Joi = require('joi');
 app.use(express.json());
 
 
@@ -20,11 +21,11 @@ app.get('/api/courses',(req, res) => {
 
 
 app.post('/api/courses', (req, res) => {
-    if (!req.body.name || req.body.length < 3 ){
-        // 400 bad request
-        res.status(400).send('Name is required and should be minimum 3 caracter.');
+    const { error } = validateCourse(req.body);
+    if (error) {
+        res.status(400).send(error.details[0].message);
         return;
-    }
+    } 
     const course = {
         id : courses.length + 1,
         name : req.body.name
@@ -32,6 +33,27 @@ app.post('/api/courses', (req, res) => {
     courses.push(course);
     res.send(course);
 });
+
+app.put('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('Curso não encontrado');
+
+    const { error } = validateCourse(req.body);
+    if (error) {
+        res.status(400).send(error.details[0].message);
+        return;
+    } 
+   course.name = req.body.name;
+   res.send(course);
+
+})
+
+function validateCourse(course){
+    const schema = {
+        name: Joi.string().min(3).required()
+    };  
+    return Joi.validate(course,schema);
+}
 
 app.get('/api/courses/:id',(req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
