945fc958ef460fb48e45f68a9d4d42734681a868
Adriano Botelho <adrianosbotelho@gmail.com>
2018-09-03 23:02:04 -0300

    Routes

--- index.js
+++ index.js
@@ -22,27 +22,23 @@ app.get('/api/courses',(req, res) => {
 
 app.post('/api/courses', (req, res) => {
     const { error } = validateCourse(req.body);
-    if (error) {
-        res.status(400).send(error.details[0].message);
-        return;
-    } 
+    if (error) return res.status(400).send(error.details[0].message);
     const course = {
         id : courses.length + 1,
         name : req.body.name
     };
     courses.push(course);
     res.send(course);
+
 });
 
 app.put('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) res.status(404).send('Curso não encontrado');
-
+    if (!course) return res.status(404).send('Curso não encontrado');
+    
     const { error } = validateCourse(req.body);
-    if (error) {
-        res.status(400).send(error.details[0].message);
-        return;
-    } 
+    if (error) return res.status(400).send(error.details[0].message);
+
    course.name = req.body.name;
    res.send(course);
 
@@ -57,13 +53,23 @@ function validateCourse(course){
 
 app.get('/api/courses/:id',(req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) res.status(404).send('Curso não encontrado');
+    if (!course) return res.status(404).send('Curso não encontrado');
     res.send(course);
 });
 
 
 app.get('/api/posts/:year/:month',(req, res) => {
     res.send(req.query);
+});
+
+app.delete('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send('Curso não encontrado');
+
+    const index = courses.indexOf(course);
+    courses.splice(index,1);
+
+    res.send(course);
 });
 
 //port
