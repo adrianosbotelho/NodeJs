df9161fc884e945c9484f4f8181053aa03586171
Adriano Botelho <adrianosbotelho@gmail.com>
2019-03-18 22:14:23 -0300

    Aula 63

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const config = require('config');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const express = require('express');
@@ -13,6 +14,11 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.use(helmet());
+
+//Configuration
+console.log('Application name: ' + config.get('name'));
+console.log('Mail Server: ' + config.get('mail.host'));
+console.log('Mail Password: ' + config.get('mail.password'));
 
 if (app.get('env') === 'development'){
     app.use(morgan('tiny'));
